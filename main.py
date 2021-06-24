1217d17d0eb888443c332b89a95e5298ba30ae02
Matteo <mateus.sakata@gmail.com>
2021-06-23 22:15:01 -0500

    Polish main.py

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 import os
 
 from scrapers.CNNScraper import CNN_Scraper
+from scrapers.WashPoScraper import WashingtonPost_Scraper
+from scrapers.PoynterScraper import Poynter_Scraper
 from ScraperManager import Scraper_Manager
 
 scrapers_dict = {
     "CNN Scraper": CNN_Scraper(
         os.path.join(os.getcwd(), "results\\CNN"), "coronavirus measures"
-    )
+    ),
+    "Washington Post Scraper": WashingtonPost_Scraper(
+        os.path.join(os.getcwd(), "results\\WashingtonPost"), "covid"
+    ),
+    "Poynter Scraper": Poynter_Scraper(os.path.join(os.getcwd(), "results\\Poynter")),
 }
 
 manager = Scraper_Manager(scrapers_dict)
